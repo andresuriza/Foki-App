503a85824af5c4219711a33f98eb553ccacf8693
Valeria <vcalvo7@outlook.com>
2026-06-18 13:42:46 -0600

    Modificacion PWA y nuevas imagenes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var fs = require('fs');
 var index = fs.readFileSync('index.html');
 
 var SerialPort = require("serialport");
-
 const parsers = SerialPort.parsers;
 const parser = new parsers.Readline({
     delimiter: '\r\n'
@@ -17,39 +16,41 @@ const parser = new parsers.Readline({
 //     stopBits: 1,
 //     flowControl: false
 // });
- 
 // port.pipe(parser);
 
 var app = http.createServer(function(req, res) {
-    // 1. Serve the main HTML page
     if (req.url === '/' || req.url === '/index.html') {
         res.writeHead(200, {'Content-Type':'text/html'});
         res.end(index);
-    } 
-    // 2. Intercept and serve image requests
-    else if (req.url.startsWith('/images/')) {
-        // Construct the file path (e.g., ./images/foki-normal.png)
-        var filePath = '.' + req.url; 
-        
-        fs.readFile(filePath, function(err, data) {
-            if (err) {
-                res.writeHead(404);
-                res.end("Image not found");
-            } else {
-                // Send the image data with the correct content type
-                res.writeHead(200, {'Content-Type': 'image/png'});
-                res.end(data);
-            }
+    }
+    // --- NEW PWA ROUTES ---
+    else if (req.url === '/manifest.json') {
+        fs.readFile('./manifest.json', function(err, data) {
+            if (err) { res.writeHead(404); res.end("Not found"); }
+            else { res.writeHead(200, {'Content-Type': 'application/manifest+json'}); res.end(data); }
         });
-    } 
-   else if (req.url.startsWith('/icons/')) {
+    }
+    else if (req.url === '/sw.js') {
+        fs.readFile('./sw.js', function(err, data) {
+            if (err) { res.writeHead(404); res.end("Not found"); }
+            else { res.writeHead(200, {'Content-Type': 'application/javascript'}); res.end(data); }
+        });
+    }
+    // ----------------------
+    else if (req.url.startsWith('/images/')) {
+        var filePath = '.' + req.url;
+        fs.readFile(filePath, function(err, data) {
+            if (err) { res.writeHead(404); res.end("Image not found"); }
+            else { res.writeHead(200, {'Content-Type': 'image/png'}); res.end(data); }
+        });
+    }
+    else if (req.url.startsWith('/icons/')) {
        var filePath = '.' + req.url;
        fs.readFile(filePath, function(err, data) {
            if (err) { res.writeHead(404); res.end("Icon not found"); }
            else { res.writeHead(200, {'Content-Type': 'image/png'}); res.end(data); }
        });
-   }
-    // 3. Handle anything else (404 Not Found)
+    }
     else {
         res.writeHead(404);
         res.end("Not found");
@@ -60,8 +61,8 @@ var io = require("socket.io")(app);
 
 io.on('connection', function(socket){
     socket.on('lights', function(data){
-        // port.write(data.status); // Commented out until you plug in the ESP32
-        console.log(data);
+        // port.write(data.status);
+        console.log("Command to Lamp:", data);
     });
 });
 
